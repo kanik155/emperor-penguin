a6b716c31b13f0c1d3fe613f446ac246a86a9909
kanik155 <takekanik3@gmail.com>
2019-11-02 21:17:10 +0900

    Naviberにアイテムを追加

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -5,31 +5,47 @@
         <a class="navbar-item" href="/">
           <img src="https://bulma.io/images/bulma-logo.png" />
         </a>
+
+        <a
+          role="button"
+          class="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
       </div>
       <div id="navbarBasicExample" class="navbar-menu">
         <div class="navbar-start">
-          <nuxt-link class="navbar-item" to="/">ホーム</nuxt-link>
+          <nuxt-link class="navbar-item" to="/">Mypage</nuxt-link>
+          <nuxt-link class="navbar-item" to="/">Settings</nuxt-link>
+          <div class="navbar-item has-dropdown is-hoverable">
+            <nuxt-link class="navbar-link" to="/">About</nuxt-link>
+            <div class="navbar-dropdown">
+              <nuxt-link class="navbar-item" to="/">利用規約</nuxt-link>
+              <nuxt-link class="navbar-item" to="/">プライバシーポリシー</nuxt-link>
+              <hr class="navbar-divider" />
+              <nuxt-link class="navbar-item" to="/">お問い合わせ</nuxt-link>
+            </div>
+          </div>
         </div>
-      </div>
-      <div class="navbar-end">
-        <div class="navbar-item">
-          <div class="buttons">
-            <!-- ここにログインボタンを⼊れる-->
-            <nuxt-link v-if="!isLogin" class="button is-light" to="/login"
-              >ログイン</nuxt-link
-            >
-            <nuxt-link v-else class="button is-light" to="/mypage"
-              >{{ userName }}さん</nuxt-link
-            >
-            <nuxt-link class="button is-primary" to="/signup">
-              <strong>アカウント作成</strong>
-            </nuxt-link>
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <nuxt-link v-if="!isLogin" class="button is-light" to="/login">ログイン</nuxt-link>
+              <nuxt-link v-else class="button is-light" to="/mypage">{{ userName }}さん</nuxt-link>
+              <nuxt-link class="button is-primary" to="/signup">
+                <strong>アカウント作成</strong>
+              </nuxt-link>
+            </div>
           </div>
         </div>
       </div>
     </nav>
     <div class="main-body">
-      <!-- これがページのレンダリング要素 -->
       <nuxt />
     </div>
     <footer class="footer">
